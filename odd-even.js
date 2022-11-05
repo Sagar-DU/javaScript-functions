60fe72fa44d0e6cd92ecb160a581bf5de309f7b1
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2022-11-06 01:10:34 +0600

    playing

---
+++ odd-even.js
@@ -0,0 +1,19 @@
+// Even or Or Check Function 
+
+function evenOrOdd (x){
+    let reminder = x % 2;
+    if (reminder == 1) {
+        console.log("The number", x ,"is an Odd number");
+    }
+    else{
+        console.log("The number", x ,"is an Even number");
+    }
+}
+
+const x = 43;
+
+// evenOrOdd(x);
+let y = 6;
+y=23;
+y=334;
+evenOrOdd(y);
