ecad891a8db16429b34498f62acd76966ba05099
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2022-11-06 04:07:43 +0600

    coding is fun when you get there!

--- factorial.js
+++ factorial.js
@@ -6,4 +6,18 @@ for (let i = 1; i <= number; i++){
     factorial = factorial * i;
 }
 
-console.log(factorial);
+// console.log(factorial);
+
+//Factorial With Function
+function factorialFunC (x){
+    let n = 1;
+    for (let i = 1; i <= x; i++) {
+        n = n*i;
+    }
+
+    return n;
+}
+
+//Factorial with funciton calling
+let x = 5;
+console.log(factorialFunC(x));
