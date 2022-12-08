4ecbecafdc8bd96437f99c403e2c2fe488b58d93
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2022-12-09 04:58:36 +0600

    code modified

--- 100years-old-year.js
+++ 100years-old-year.js
@@ -1,5 +1,11 @@
 let userName = "Sagar", age = 26;
 
-let hundredthYear = 2022 + (100 - (age));
+const date = new Date();
+
+let year = date.getFullYear();
+
+let hundredthYear = year + (100 - (age));
 
 console.log (userName, "will be 100 years old in:", hundredthYear);
+
+// console.log(year);
