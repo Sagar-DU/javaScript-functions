dc4b72d144cf0ab1bcb1b4f5d1561439b571307a
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2023-01-23 23:58:58 +0600

    math related code added

---
+++ math.js
@@ -0,0 +1,22 @@
+let number = -23;
+
+// const output = Math.abs(number);
+
+number = 2.523;
+
+// const output = Math.ceil(number);
+// const output = Math.floor(number);
+
+number = 2.5;
+
+// number = Math.random() * 10;
+
+// const output = Math.round(number);
+
+
+for (let i = 0; i < 30;i++) {
+    number = Math.random() * 6;
+    const output = Math.ceil(number);
+    console.log(output);
+    
+}
