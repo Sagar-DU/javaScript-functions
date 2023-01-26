ae4c900efa3456dfe3f930d3997739a12d0a6b16
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2023-01-26 08:52:09 +0600

    new code stated

---
+++ max-min.js
@@ -0,0 +1,7 @@
+let doctor = 234, engineer = 544, businessman = 451;
+
+let outputMax = Math.max(doctor, engineer, businessman);
+let outputMin = Math.min(doctor, engineer, businessman);
+
+console.log("Minimum wage:",outputMin);
+console.log("Maximum wage:",outputMax);
