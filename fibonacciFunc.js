505adaf968ee30e90629e4e22d549e6d150c01ab
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2023-01-30 21:12:19 +0600

    fibonacci function done!

--- fibonacciFunc.js
+++ fibonacciFunc.js
@@ -1,1 +1,19 @@
 //Write a function for Fibonacci series
+function fibonacci(n) {
+    let x = [0, 1];
+    if (n == 1) {
+        return 0;
+    }
+    else if (n == 2) {
+        return x;
+    }
+    else if (n > 2) {
+        for (let i = 2; i < n; i++) {
+            x[i] = x[i-2] + x[i-1]; 
+        }
+        return x;
+    }
+    return "Please enter a valid input."
+}
+
+console.log(fibonacci(100));
